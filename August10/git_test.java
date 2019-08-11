906059ba85e5435efd6e09fa7ad698eae1a24476
Sechang <scoh.bears@gmail.com>
2019-08-10 22:24:39 -0700

    change text

--- August10/git_test.java
+++ August10/git_test.java
@@ -2,6 +2,6 @@ package August10;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("Git Test!");
+        System.out.println("Hello World!");
     }
 }
